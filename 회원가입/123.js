628edd4c383e7232b5b331e055b6f7a4c2dd98c6
Jang Gyeoungguk <97073355+rnr9928@users.noreply.github.com>
2022-07-11 17:29:31 +0900

    Add files via upload

---
+++ 회원가입/123.js
@@ -0,0 +1,15 @@
+const form=document.getElementById('form');
+c
+c
+c
+c
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    checkInput();
+})
+
+function checkInput() {
+const Value
+}
